a8ac549518aed076f4fbbbe3267234b3dbf8890c
Robert Kanyur <robert@kanyur.com>
2019-11-12 15:28:14 -0700

    Starting to add ability to calculate quality of game

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import League, Team, Game
+
+admin.site.register(League)
+admin.site.register(Team)
+admin.site.register(Game)
--- manager/models.py
+++ manager/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import math
 
 class League(models.Model):
     name = models.CharField(max_length=50)
@@ -13,7 +13,8 @@ class Team (models.Model):
     location = models.CharField(max_length=50)
     nickname = models.CharField(max_length=50)
     abbreviation = models.CharField(max_length=20)
-    league = models.ForeignKey(League, null=True, on_delete=models.SET_NULL)
+    league = models.ForeignKey(League, null=True, on_delete=models.SET_NULL, 
+                               related_name="team")
     championship_odds = models.IntegerField(default=0)
 
     def display_name(self):
@@ -27,5 +28,10 @@ class Game (models.Model):
     team = models.ForeignKey(Team, null=True, on_delete=models.SET_NULL)
     opponent = models.ForeignKey(Team, null=True, on_delete=models.SET_NULL, 
                                  related_name="+")
-    game_date = models.DateTimeField()
-    game_quality = models.IntegerField()
+    game_date = models.DateTimeField(null=True)
+
+    def game_quality(self):
+        home_index = 1000000 - self.team.championship_odds
+        away_index = 1000000 - self.opponent.championship_odds
+
+        return math.floor((home_index * away_index) / 10000000000)
